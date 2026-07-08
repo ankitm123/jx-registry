3cc8c3c58b3ce280828e690f011a4be14673c498
Test <mentlak@outlook.com>
2026-07-08 13:30:54 +0100

    chore: update golangci version in linter.sh

--- hack/linter.sh
+++ hack/linter.sh
@@ -11,7 +11,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if ! [ -x "$(command -v golangci-lint)" ]; then
 	echo "Installing GolangCI-Lint"
-	${DIR}/install_golint.sh -b $GOPATH/bin v2.12.0
+	${DIR}/install_golint.sh -b $GOPATH/bin v2.12.2
 fi
 
 export GO111MODULE=on
